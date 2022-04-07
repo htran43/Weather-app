d40e09fc9c23687871f0ecabcb56e87d3789c746
Fido27 <42429320+Fido27@users.noreply.github.com>
2022-04-07 14:29:54 -0700

    Add files via upload

---
+++ WeatherApp/WeatherApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by FSE394 on 4/7/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
